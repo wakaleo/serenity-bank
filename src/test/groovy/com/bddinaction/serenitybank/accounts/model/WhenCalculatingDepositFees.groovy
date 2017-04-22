bbec348d6e02879edca1ab92385becaa12399dc0
John Smart <john.smart@wakaleo.com>
2017-04-22 10:31:55 +0100

    Added a sample Spock specification for deposit fee calculations

---
+++ src/test/groovy/com/bddinaction/serenitybank/accounts/model/WhenCalculatingDepositFees.groovy
@@ -0,0 +1,44 @@
+package com.bddinaction.serenitybank.accounts.model
+
+import com.bddinaction.serenitybank.accounts.AccountService
+import com.bddinaction.serenitybank.deposits.DepositFee
+import com.bddinaction.serenitybank.model.AccountType
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.bddinaction.serenitybank.model.AccountType.BasicSavings
+import static com.bddinaction.serenitybank.model.AccountType.BasicSavings
+import static com.bddinaction.serenitybank.model.AccountType.BasicSavings
+import static com.bddinaction.serenitybank.model.AccountType.BasicSavings
+import static com.bddinaction.serenitybank.model.AccountType.BasicSavings
+import static com.bddinaction.serenitybank.model.AccountType.BasicSavings
+import static com.bddinaction.serenitybank.model.AccountType.BigSaver
+import static com.bddinaction.serenitybank.model.AccountType.Current
+import static com.bddinaction.serenitybank.model.AccountType.Current
+
+class WhenCalculatingDepositFees extends Specification {
+
+    @Unroll
+    def "a deposit fee of #expectedDepositFee should be charged when depositing €#amount into a #accountType account"() {
+        expect:
+        DepositFee.forAccountType(accountType).apply(amount) == expectedDepositFee
+        where:
+
+        accountType  | amount  | expectedDepositFee
+        Current      | 1000.0  | 0.0
+        // Basic savings
+        BasicSavings | 10.00   | 0.50
+        BasicSavings | 100.00  | 0.50
+        BasicSavings | 100.01  | 1.00
+        BasicSavings | 1000.00 | 1.00
+        BasicSavings | 1000.01 | 1.25
+        BasicSavings | 1500.00 | 1.25
+        // Big Saver
+        BigSaver     | 10.00   | 0.50
+        BigSaver     | 100.00  | 0.50
+        BigSaver     | 100.01  | 0.75
+        BigSaver     | 1000.00 | 0.75
+        BigSaver     | 1000.01 | 1.25
+        BigSaver     | 1500.00 | 1.25
+    }
+}
