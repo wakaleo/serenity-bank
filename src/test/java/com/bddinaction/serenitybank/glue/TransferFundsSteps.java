efc33496b759fa7652916f9cf25f6b90b95e053e
John Smart <john.smart@wakaleo.com>
2017-04-22 07:51:18 +0100

    Demoing some Gherkin constructs

--- src/test/java/com/bddinaction/serenitybank/glue/TransferFundsSteps.java
+++ src/test/java/com/bddinaction/serenitybank/glue/TransferFundsSteps.java
@@ -5,13 +5,15 @@ import com.bddinaction.serenitybank.accounts.AccountService;
 import com.bddinaction.serenitybank.model.AccountType;
 import com.google.common.collect.Maps;
 import cucumber.api.PendingException;
+import cucumber.api.Transform;
+import cucumber.api.Transformer;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.assertj.core.api.SoftAssertions;
-import org.assertj.core.data.Percentage;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,7 +38,9 @@ public class TransferFundsSteps {
     }
 
     @When("^he transfers €(\\d+) from his (.*) account to his (.*) account$")
-    public void transfersFunds(BigDecimal amount, AccountType fromAccountType, AccountType toAccountType) throws Throwable {
+    public void transfersFunds(BigDecimal amount,
+                               AccountType fromAccountType,
+                               AccountType toAccountType) throws Throwable {
         AccountDetails fromAccount = accountsByType.get(fromAccountType);
         AccountDetails toAccount = accountsByType.get(toAccountType);
 
@@ -62,5 +66,87 @@ public class TransferFundsSteps {
                                                      AccountType type) throws Throwable {
         AccountDetails fromAccount = accountsByType.get(type);
         accountService.makeDeposit(fromAccount.getNumber(), amount);
+    }
+
+    @Given("^the distance from (.*) to (.*) is (\\d+) km$")
+    public void pointsEarnedBetweenCities(String departure,
+                                          String destination,
+                                          int distance) throws Throwable {
+    }
+
+    public enum City {
+        London("London"), Paris("Paris"), SanFrancisco("San Francisco"), SaintPetersbuug("St Petersburg");
+
+        public final String name;
+
+        City(String name) {
+            this.name = name;
+        }
+    }
+
+    public class CityConverter  extends Transformer<City> {
+        @Override
+        public City transform(String cityName) {
+            return Arrays.stream(City.values())
+                    .filter(city -> city.name.equalsIgnoreCase(cityName))
+                    .findFirst()
+                    .orElseThrow(() -> new UnknownCityException(cityName));
+        }
+    }
+
+    public class UnknownCityException extends RuntimeException {
+        public UnknownCityException(String message) {
+            super(message);
+        }
+    }
+
+    public enum CabinClass {
+        Economy, Business, First
+    }
+
+    public class Itinerary {
+        public final City departure;
+        public final City destination;
+        public final int distance;
+
+        public Itinerary(City departure, City destination, int distance) {
+            this.departure = departure;
+            this.destination = destination;
+            this.distance = distance;
+        }
+    }
+
+    @Given("the following flight routes:")
+    public void flightRoutes(List<Itinerary> flightRoutes) {
+    }
+
+    @When("^I fly from (.*) to (.*) in (.*)$")
+    public void iFly(@Transform(CityConverter.class) City departure, City destination, CabinClass cabinClass) throws Throwable {
+    }
+
+    @Then("^I should earn (\\d+) points$")
+    public void iShouldEarnPoints(int arg0) throws Throwable {
+    }
+
+    @Given("^Joe has a <account-type> account with an initial balance of <initial-balance>$")
+    public void joeHasAAccountTypeAccountWithAnInitialBalanceOfInitialBalance() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^he deposits €<deposit> into his account$")
+    public void heDeposits€DepositIntoHisAccount() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^his account should have a balance of €<final-balance>$")
+    public void hisAccountShouldHaveABalanceOf€FinalBalance() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^the following possible destinations: (.*)$")
+    public void theFollowingPossibleDestinations(List<City> destinations) {
     }
 }
--- src/test/resources/features/deposit_funds/make_a_cash_deposit.feature
+++ src/test/resources/features/deposit_funds/make_a_cash_deposit.feature
@@ -15,7 +15,7 @@ Feature: Make a cash deposit
       | Number | Type    | Balance |
       | 123456 | Current | 1100    |
 
-  Scenario: Deposit into a basic savings account
+  Scenario: Deposit small amounts into a basic savings account
   For all savings accounts, there is a €0.50 fee up to €100.
 
     Given Joe has the following accounts:
@@ -26,7 +26,7 @@ Feature: Make a cash deposit
       | Number | Type         | Balance |
       | 123456 | BasicSavings | 1049.50 |
 
-  Scenario: Deposit into a super savings account
+  Scenario: Deposit small amounts into a super savings account
   For all savings accounts, there is a €0.50 fee up to €100.
 
     Given Joe has the following accounts:
@@ -36,3 +36,36 @@ Feature: Make a cash deposit
     Then he should have the following balances:
       | Number | Type     | Balance |
       | 123456 | BigSaver | 1099.50 |
+
+  Scenario: Economy flights earn points by distance
+    Given the distance from London to Paris is 344 km
+    Given the following possible destinations: London, Paris, Amsterdam
+    When I fly from London to Paris in Economy
+    Then I should earn 344 points
+
+
+  Scenario Outline:
+    Given the following flight routes:
+      | departure | destination | distance |
+      | London    | Paris       | 344      |
+      | Paris     | Krakow      | 1500     |
+      | London    | Budapest    | 1600     |
+    When I fly from <departure> to <destination> in Economy
+    Then I should earn <points> points
+    And I should earn <status-points> status points
+
+    Examples:
+      | departure | destination | points | status-points |
+      | London    | Paris       | 344    | 3             |
+      | Paris     | Krakow      | 1500   | 15            |
+      | London    | Budapest    | 1600   | 16            |
+
+
+#
+#  Scenario Outline: Deposit larger cash deposits
+#    Given Joe has a <account-type> account with an initial balance of <initial-balance>
+#    When he deposits €<deposit> into his account
+#    Then his account should have a balance of €<final-balance>
+#    Examples:
+#      | account-type | initial-balance | deposit | final-balance | rule                       |
+#      | BasicSavings | 1000            | 200     | 1199          | €1.00 fee for basic savers |
